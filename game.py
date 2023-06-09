0239772ab90d002b44cbaa8660406524cab63975
madao_km <madaokm@hotmail.com>
2023-06-09 15:39:40 -0400

    Day 10: Game test w/o AI

--- game.py
+++ game.py
@@ -3,109 +3,39 @@ from sys import exit
 from random import randint,choice
 from time import time
 
-# Recycle Bin
-
-# self.row_1 = [0 + self.tile_img.get_rect(center = (450+100*x, 800)) for x in range(8)]
-# self.row_2 = [0 + self.tile_img.get_rect(center = (450+100*x, 700)) for x in range(8)]
-# self.row_3 = [0 + self.tile_img.get_rect(center = (450+100*x, 600)) for x in range(8)]
-# self.row_4 = [0 + self.tile_img.get_rect(center = (450+100*x, 500)) for x in range(8)]
-# self.row_5 = [0 + self.tile_img.get_rect(center = (450+100*x, 400)) for x in range(8)]
-# self.row_6 = [0 + self.tile_img.get_rect(center = (450+100*x, 300)) for x in range(8)]
-# self.row_7 = [0 + self.tile_img.get_rect(center = (450+100*x, 200)) for x in range(8)]
-# self.row_8 = [0 + self.tile_img.get_rect(center = (450+100*x, 100)) for x in range(8)]
-
-# self.board = [0, self.row_1, self.row_2, self.row_3, self.row_4, self.row_5, self.row_6, self.row_7, self.row_8]
-
-# piece stuff
-# if (750, 400) coord should be row 5 (3, 5)
-# self.x_coord = ((rect.x - 450)//100)+1 
-# self.y_coord = ((rect.y - 100)//-100)+8
-# if player == True:
-#     self.b = pygame.image.load('graphics/tile_b.png').convert_alpha()
-#     self.b_rect = rect
-#     # convert rect to list index
-#     # change valie to tile with piece
-#     # (3, 5) -> (750, 400)
-#     self.board[self.x_coord][self.y_coord] = self.b.get_rect(center = (450+100*self.x_coord,))
-
-#     self.piece_w = pygame.image.load('graphics/tile_w.png').convert_alpha()
-#     self.w_rect = rect
-
-# self.pieces = [self.piece_b.get_rect(center = (750,400)), self.piece_b.get_rect(center = (850, 500))]
-
-# class Piece(pygame.sprite.Sprite):
-#     def __init__(self, type, rect):
-#         super().__init__()
-
-#         if type == 'player':
-#             self.flipped = False
-#             self.img = pygame.image.load('graphics/piece_1.png').convert_alpha()
-#             self.rect = self.img.get_rect(center = rect)
-
-#         else:
-#             self.flipped = True
-#             self.img = pygame.image.load('graphics/piece_2.png').convert_alpha()
-#             self.rect = self.img.get_rect(center = rect)
-
-#         self.x_coord = ((self.rect.x - 450)//100)+1
-#         self.y_coord = ((self.rect.y - 100)//-100)+8
-#         self.position = (self.x_coord,self.y_coord)
-
-#         '''
-#         x -> 450,1150
-#         y -> 100, 800
-
-#         (1,1)
-#         400,50 would be the bottom left
-#         X: Examples (450, 100), (1150,800)
-#         -450 (0)  (700)
-#         /100 (0)  (7)
-#         +1   (1)  (8)
-
-#         Y: Examples (450, 100), (1150,800)
-#         -100  (0)  (700)
-#         /-100 (0)  (-7)
-#         +8    (8)  (1)
-#         '''
-#     def display_piece(self):
-#         screen.blit(self.img,self.rect)
-
-#     def update(self):
-#         self.display_piece()
-
 class Player:
     def __init__(self):
         super().__init__()
         global event, player_turn, board
         self.turn = 0
-        self.turn_display = arial_font.render(f'Turn: {self.turn}',False,'Black')
+        self.turn_display = arial_font_40.render(f'Turn: {self.turn}',False,'Black')
         self.turn_display_rect = self.turn_display.get_rect(center = (200,400))
 
         self.piece_num = 0
-        self.piece_num_display = arial_font.render(f'Pieces: {self.piece_num}',False,'Black')
+        self.piece_num_display = arial_font_40.render(f'Pieces: {self.piece_num}',False,'Black')
         self.piece_num_rect = self.piece_num_display.get_rect(center = (200,500))
 
         if player_turn:
-            self.turn_side = arial_font.render(f'Turn: Player',False,'Black')
+            self.turn_side = arial_font_40.render(f'Turn: Black',False,'Black')
         else:
-            self.turn_side = arial_font.render(f'Turn: Opponent',False,'Black')
+            self.turn_side = arial_font_40.render(f'Turn: White',False,'Black')
         self.turn_side_rect = self.turn_side.get_rect(center = (200,600))
 
     def display_turn(self):
-        self.turn_display = arial_font.render(f'Turn: {self.turn}',False,'Black')
+        self.turn_display = arial_font_40.render(f'Turn: {self.turn}',False,'Black')
         screen.blit(self.turn_display,self.turn_display_rect)
         return self.turn
     
     def display_side(self):
         if player_turn:
-            self.turn_side = arial_font.render(f'Turn: Player',False,'Black')
+            self.turn_side = arial_font_40.render(f'Turn: Black',False,'Black')
         else:
-            self.turn_side = arial_font.render(f'Turn: Opponent',False,'Black')
+            self.turn_side = arial_font_40.render(f'Turn: White',False,'Black')
         self.turn_side_rect = self.turn_side.get_rect(center = (200,600))
         screen.blit(self.turn_side,self.turn_side_rect)
 
     def display_piece_num(self):
-        self.piece_num_display = arial_font.render(f'Pieces: {self.get_pieces()}',False,'Black')
+        self.piece_num_display = arial_font_40.render(f'Pieces: {self.get_pieces()}',False,'Black')
         screen.blit(self.piece_num_display,self.piece_num_rect)
 
     def get_pieces(self):
@@ -718,22 +648,22 @@ circles2 = []
 
 title = pygame.image.load('graphics/game_title.png').convert_alpha()
 title_rect = title.get_rect(center = (800,150))
-arial_font_2 = pygame.font.Font('font/Arialn.ttf',50)
-play_msg = arial_font_2.render('Tap to play!',True,'Red')
+arial_font_50 = pygame.font.Font('font/Arialn.ttf',50)
+play_msg = arial_font_50.render('Tap to play!',True,'Red')
 play_msg_rect = play_msg.get_rect(midtop = (800, 450))
-exit_button = arial_font_2.render('X',True,'Red')
+exit_button = arial_font_50.render('X',True,'Red')
 exit_rect = exit_button.get_rect(topright = (1580,20))
 
 # RULES VARIABLES
 
 rules = pygame.image.load('graphics/rules.png').convert()
 rules_rect = rules.get_rect(center = (120,70))
-arial_font = pygame.font.Font('font/Arialn.ttf',40)
-rules_text_1 = arial_font.render('1. Pieces can be placed as long as there are at least 1 adjacent enemy piece',True,'Black')
-rules_text_2 = arial_font.render('2. When placing a piece, opposing pieces that are surrounded vertically, horizontally,',True,'Black')
-rules_text_3 = arial_font.render('    and diagonally by two ends including the placed piece are flipped',True,'Black')
-rules_text_4 = arial_font.render('3. Player with the most remaining pieces win the game',True,'Black')
-rules_text_5 = arial_font.render('4. The 4 corner pieces cannot be flipped (tip)',True,'Black')
+arial_font_40 = pygame.font.Font('font/Arialn.ttf',40)
+rules_text_1 = arial_font_40.render('1. Pieces can be placed as long as there are at least 1 adjacent enemy piece',True,'Black')
+rules_text_2 = arial_font_40.render('2. When placing a piece, opposing pieces that are surrounded vertically, horizontally,',True,'Black')
+rules_text_3 = arial_font_40.render('    and diagonally by two ends including the placed piece are flipped',True,'Black')
+rules_text_4 = arial_font_40.render('3. Player with the most remaining pieces win the game',True,'Black')
+rules_text_5 = arial_font_40.render('4. The 4 corner pieces cannot be flipped (tip)',True,'Black')
 rules_texts = [rules_text_1,rules_text_2,rules_text_3,rules_text_4,rules_text_5]
 rules_text_rect = rules_text_1.get_rect(midleft = (100, 300))
 
@@ -758,12 +688,6 @@ surrender_rect = surrender_surf.get_rect(center = (120,70))
 skip_surf = pygame.image.load('graphics/skip.png').convert()
 skip_rect = skip_surf.get_rect(center = (120,190))
 
-piece_b = pygame.image.load('graphics/piece_1.png').convert_alpha()
-piece_b_rect = piece_b.get_rect(center = (750,400))
-
-piece_w = pygame.image.load('graphics/piece_2.png').convert_alpha()
-piece_w_rect = piece_b.get_rect(center = (750,500))
-
 player = Player()
 
 board = []
@@ -778,11 +702,13 @@ for x in range(1,9):
 
 # Score screen variables
 
+arial_font_100 = pygame.font.Font('font/Arialn.ttf',100)
+
 retry_surf = pygame.image.load('graphics/retry.png').convert()
-retry_rect = retry_surf.get_rect(center = (400,700))
+retry_rect = retry_surf.get_rect(center = (600,700))
 
 exit_to_menu = pygame.image.load('graphics/menu.png').convert()
-menu_exit_rect = exit_to_menu.get_rect(center = (1200,700))
+menu_exit_rect = exit_to_menu.get_rect(center = (1000,700))
 
 circle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(circle_timer, 600)
@@ -918,23 +844,24 @@ while True:
         for x in board:
             x.update()
             x.display()
+
     if score_screen:
         if player.get_pieces() == player.get_opp_pieces():
             #draw 
             screen.fill('Yellow')
-            end_msg = arial_font.render(f'Draw!',True,'White')
+            end_msg = arial_font_100.render(f'Draw!',True,'White')
             pass
         elif player.get_pieces() > player.get_opp_pieces():
             #win
             screen.fill('Green')
-            end_msg = arial_font.render(f'Player won with a difference of {player.get_pieces() - player.get_opp_pieces()}',True,'White') 
+            end_msg = arial_font_100.render(f'Player won with a difference of {player.get_pieces() - player.get_opp_pieces()}',True,'White') 
             pass
         else:
             #loss
             screen.fill('Red')
-            end_msg = arial_font.render(f'Player lost with a difference of {player.get_opp_pieces() - player.get_pieces()}',True,'White')
+            end_msg = arial_font_100.render(f'Player lost with a difference of {player.get_opp_pieces() - player.get_pieces()}',True,'White')
             pass
-        screen.blit(end_msg,end_msg.get_rect(center = (800,450)))
+        screen.blit(end_msg,end_msg.get_rect(center = (800,400)))
         screen.blit(retry_surf,retry_rect)
         screen.blit(exit_to_menu,menu_exit_rect)
     pygame.display.update()
